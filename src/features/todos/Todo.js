51f8ccbea2f407af2b8b16f01bc219f14d3787f4
bayogluteoman <bayogluteoman@gmail.com>
2022-03-05 02:20:47 +0300

    delete items with Redux

--- src/features/todos/Todo.js
+++ src/features/todos/Todo.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { todoRemoved } from "./todosSlice";
 
-function Todo({ text }) {
-  return <li>{text}</li>;
+function Todo({ todo }) {
+  const dispatch = useDispatch();
+
+  function handleDeleteClick() {
+    dispatch(todoRemoved(todo.id));
+  }
+
+  return (
+    <li style={{marginBottom: 10}}>
+      <span>{todo.text}</span>
+      <button  onClick={handleDeleteClick} style={{marginLeft: 10, color:"black", backgroundColor: "red", borderRadius: 10}}>DELETE</button>
+    </li>
+  );
 }
 
 export default Todo;
